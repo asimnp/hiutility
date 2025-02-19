ec60bc8b4abff7494be84cbdf302c483ab67dc12
asim <asimshrestha608@gmail.com>
2025-02-19 22:11:33 +0545

    refactor: routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,35 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 import "./index.css";
 import App from "./App.jsx";
-import Home from "./pages/Home.jsx";
 import Tools from "./pages/Tools.jsx";
 import ToolsProvider from "./context/ToolsProvider.jsx";
-import ToolSingle from "./pages/ToolSingle.jsx";
-import ToolsLayout from "./pages/layouts/ToolsLayout.jsx";
+import HomePage from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>404 page not found</div>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/tools",
+        element: <Tools />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ToolsProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="" element={<Home />} />
-          </Route>
-
-          <Route path="tools" element={<ToolsLayout />}>
-            <Route path="" element={<Tools />} />
-            <Route path=":toolSlug" element={<ToolSingle />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ToolsProvider>
   </StrictMode>
 );
