39d646dd650cbbc0b1121672ec6086d56e72a874
asim <asimshrestha608@gmail.com>
2025-02-21 09:37:39 +0545

    Improve Input component for better reusability

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,9 @@
-export default function Input({
-  type = "text",
-  placeholder,
-  amount,
-  setAmount,
-}) {
+export default function Input({ type = "text", placeholder, state, setState }) {
   return (
     <input
       type={type}
-      value={amount}
-      onChange={(e) => setAmount(e.target.value)}
+      value={state}
+      onChange={(e) => setState(e.target.value)}
       placeholder={placeholder}
       className="p-3 border rounded-md my-5 border-gray-200 w-full"
     />
