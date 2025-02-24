ac900390e64fdd18f32015d93a4ebfab56111edf
asim <asimshrestha608@gmail.com>
2025-02-24 21:48:05 +0545

    feat: search tools

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { LuSearch } from "react-icons/lu";
 
-export default function SearchBar() {
+export default function SearchBar({ value, setValue }) {
   return (
     <form className="flex justify-center items-center ">
       <label htmlFor="" className="relative">
@@ -11,6 +11,8 @@ export default function SearchBar() {
 
         <input
           type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
           placeholder="Search for utilities..."
           className="border-2 border-gray-100 h-12 rounded-md w-xl pl-14 focus:outline-red-600"
         />
--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -1,11 +1,22 @@
+import { useState, useEffect } from "react";
+
 import SearchBar from "../components/SearchBar";
 import ToolCard from "../components/ToolCard";
 import { useTools } from "../context/tools";
+import { useMemo } from "react";
 
 export default function Tools() {
   const { tools } = useTools();
+  const [search, setSearch] = useState("");
 
-  const sortedTools = tools.toSorted((a, b) => a.name.localeCompare(b.name));
+  let sortedTools = tools.toSorted((a, b) => a.name.localeCompare(b.name));
+
+  const filteredTools = useMemo(() => {
+    return sortedTools.filter(
+      (todo) =>
+        search === "" || todo.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [search, sortedTools]);
 
   return (
     <div className="container mx-auto">
@@ -13,11 +24,11 @@ export default function Tools() {
         <h1 className="font-bold text-2xl">
           Explore <span className="text-red-600">utility</span> tools
         </h1>
-        <SearchBar />
+        <SearchBar value={search} setValue={setSearch} />
       </div>
 
       <div className="grid grid-cols-3 gap-7 mt-18 mb-52">
-        {sortedTools.map((tool) => (
+        {filteredTools.map((tool) => (
           <ToolCard key={tool.path} tool={tool} />
         ))}
       </div>
