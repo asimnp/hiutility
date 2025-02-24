d4745a517e2ef157d7db2aed00ad1e71d0741d3d
asim <asimshrestha608@gmail.com>
2025-02-24 21:36:38 +0545

    refactor: home ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,12 @@ import About from "../components/About";
 import Contact from "../components/Contact";
 import Hero from "../components/Hero";
 import NewTools from "../components/NewTools";
-import SearchBar from "../components/SearchBar";
 
 export default function HomePage() {
   return (
     <>
       <div className="container mx-auto mt-3">
         <Hero />
-        <SearchBar />
         <NewTools />
       </div>
       <About />
