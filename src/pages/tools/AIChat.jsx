1ce3e681d302dd7d0d7f747b52a555139ecce348
asim <asimshrestha608@gmail.com>
2025-02-25 11:40:47 +0545

    create pages for two new tools

---
+++ src/pages/tools/AIChat.jsx
@@ -0,0 +1,14 @@
+import ToolHeader from "../../components/ToolHeader";
+
+export default function AIChat() {
+  return (
+    <div className="container mx-auto">
+      <ToolHeader
+        name="AI Chat"
+        description="An intelligent and responsive chat assistant designed to provide instant answers, personalized recommendations, and engaging conversations."
+      />
+
+      <div className="text-gray-400 my-10">Coming soon...</div>
+    </div>
+  );
+}
---
+++ src/pages/tools/Weather.jsx
@@ -0,0 +1,14 @@
+import ToolHeader from "../../components/ToolHeader";
+
+export default function Weather() {
+  return (
+    <div className="container mx-auto">
+      <ToolHeader
+        name="Weather"
+        description="A smart weather application that delivers real-time forecasts, severe weather alerts, and climate insights. "
+      />
+
+      <div className="text-gray-400 my-10">Coming soon...</div>
+    </div>
+  );
+}
