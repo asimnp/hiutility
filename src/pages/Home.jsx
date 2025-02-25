6b6aa2de214c4b9cbfd3dbc70ef5a1cbb854a630
asim <asimshrestha608@gmail.com>
2025-02-25 12:13:01 +0545

    fix hash links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
+import { useLocation } from "react-router";
+import { useEffect } from "react";
+
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Hero from "../components/Hero";
 import NewTools from "../components/NewTools";
 
 export default function HomePage() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.querySelector(location.hash);
+
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
   return (
     <>
       <div className="container mx-auto mt-3">
