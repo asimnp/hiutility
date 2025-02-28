1c4e0dd7f5f09546e52a16be54460c1ae2a03e9f
asim <asimshrestha608@gmail.com>
2025-02-28 20:27:59 +0545

    Get weather data based on city

---
+++ src/components/weather/CurrentWeatherCard.jsx
@@ -0,0 +1,115 @@
+import { HiOutlineMapPin, HiOutlineClock, HiOutlineEye } from "react-icons/hi2";
+import { CiTempHigh } from "react-icons/ci";
+import { PiWind } from "react-icons/pi";
+import { LuDroplets } from "react-icons/lu";
+import { BsSunrise, BsSunset } from "react-icons/bs";
+
+export default function CurrentWeatherCard({ data }) {
+  return (
+    <section className="w-6xl mb-24">
+      <div className="flex justify-between items-center bg-gray-800 text-white px-10 py-4 rounded-t-md">
+        <div className="flex flex-col gap-5">
+          <div className="flex gap-5 items-center">
+            <HiOutlineMapPin size={40} />
+            <div className="text-2xl font-medium">{data.name}</div>
+          </div>
+          <div className="flex justify-center items-center gap-2">
+            <p>Lat: {data.coord.lat}</p> &middot;
+            <p>Lon: {data.coord.lon}</p>
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center gap-3">
+          <HiOutlineClock size={40} />
+          <p className="text-xl">{data.timezone}</p>
+        </div>
+      </div>
+
+      <div className="bg-gray-50 px-10 py-20 flex justify-between items-center">
+        <div className="flex justify-center items-center gap-3">
+          <img
+            src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+            alt={`${data.weather[0].description} weather`}
+            className="w-25 h-25"
+          />
+          <div className="flex flex-col justify-center items-center gap-3">
+            <div className="text-5xl font-bold">
+              {data.main.temp}
+              <sup>o</sup>C
+            </div>
+            <div className="text-xl text-gray-600">
+              {data.weather[0].description}
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-8">
+          <div className="flex items-center gap-3">
+            <CiTempHigh size={25} className="text-amber-500" />
+
+            <div>
+              <div className="text-sm text-gray-600">Feels like</div>
+              <div className="font-bold">
+                {data.main.feels_like}
+                <sup>o</sup>C
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <PiWind size={25} className="text-blue-500" />
+
+            <div>
+              <div className="text-sm text-gray-600">Wind speed</div>
+              <div className="font-bold">{data.wind.speed} mph</div>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <HiOutlineEye size={25} className="text-purple-600" />
+
+            <div>
+              <div className="text-sm text-gray-600">Visibility</div>
+              <div className="font-bold">{data.visibility / 1000} km</div>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <LuDroplets size={25} className="text-blue-500" />
+
+            <div>
+              <div className="text-sm text-gray-600">Humidity</div>
+              <div className="font-bold">{data.main.humidity}%</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-amber-100 p-5 rounded-b-md">
+        <div className="ml-10 text-xl mb-5 font-medium">Sun Schedule</div>
+
+        <div className="flex justify-between items-center gap-5 mx-10">
+          <div className="flex justify-center items-center gap-5">
+            <BsSunrise size={30} className="text-amber-600" />
+            <div>
+              <div className="text-sm text-gray-600">Sunrise</div>
+              {/* <div>{data.sys.sunrise}</div> */}
+              <div className="font-bold text-lg">09:45 AM</div>
+            </div>
+          </div>
+
+          <hr className="w-xl h-1.5 border-0 rounded-md bg-linear-to-r from-amber-300 to-amber-600" />
+
+          <div className="flex justify-center items-center gap-5">
+            <BsSunset size={30} className="text-amber-600" />
+            <div>
+              <div className="text-sm text-gray-600">Sunset</div>
+              {/* <div>{data.sys.sunset}</div> */}
+              <div className="font-bold text-lg">12:05 AM</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/tools/Weather.jsx
+++ src/pages/tools/Weather.jsx
@@ -1,6 +1,30 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 import ToolHeader from "../../components/ToolHeader";
+import envVars from "../../config/env";
+import { useState } from "react";
+import CurrentWeatherCard from "../../components/weather/CurrentWeatherCard";
 
 export default function Weather() {
+  const [data, setData] = useState(null);
+  const [city, setCity] = useState("kathmandu");
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${envVars.WEATHER_API_KEY}`
+      );
+      setData(response.data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="container mx-auto">
       <ToolHeader
@@ -8,7 +32,11 @@ export default function Weather() {
         description="A smart weather application that delivers real-time forecasts, severe weather alerts, and climate insights. "
       />
 
-      <div className="text-gray-400 my-10">Coming soon...</div>
+      {data ? (
+        <CurrentWeatherCard data={data} />
+      ) : (
+        <div className="text-gray-400 my-10">Loading current weather...</div>
+      )}
     </div>
   );
 }
