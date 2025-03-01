c00f2a2eac461a2191056df8921eba172db1c4bb
asim <asimshrestha608@gmail.com>
2025-03-01 13:30:08 +0545

    Setup prettier Tailwind CSS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 export default function Hero() {
   return (
-    <main className="flex flex-col justify-center items-center mt-25">
-      <div className="border-2 border-red-600 rounded-lg shadow-lg dark:shadow-red-500 p-20">
+    <main className="mt-25 flex flex-col items-center justify-center">
+      <div className="rounded-lg border-2 border-red-600 p-20 shadow-lg dark:shadow-red-500">
         <div className="text-6xl font-bold text-red-600">
           Your Ultimate <span className="text-black">Utility Toolkit</span>
         </div>
-        <p className="text-xl my-5 text-gray-800 dark:text-gray-300">
+        <p className="my-5 text-xl text-gray-800 dark:text-gray-300">
           Discover a comprehensive collection of utility tools designed to
           simplify your daily tasks.
         </p>
