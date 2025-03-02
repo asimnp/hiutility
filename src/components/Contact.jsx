e10e1a251ee0251a8ecee0d06919615c17251d1d
asim <asimshrestha608@gmail.com>
2025-03-02 15:04:14 +0545

    Setup contact us form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,39 @@
-import { LuMail } from "react-icons/lu";
-import { FaXTwitter } from "react-icons/fa6";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import envVars from "../config/env.js";
+import { useState } from "react";
 
 export default function Contact() {
+  const [message, setMessage] = useState("");
+  const form = useRef();
+  const [sending, setSending] = useState(false);
+
+  const handleEmailSend = (e) => {
+    e.preventDefault();
+    setSending(true);
+
+    emailjs
+      .sendForm(
+        envVars.EMAIL_SERVICE_ID,
+        envVars.EMAIL_TEMPLATE_ID,
+        form.current,
+        {
+          publicKey: envVars.EMAIL_PUBLIC_KEY,
+        }
+      )
+      .then(() => {
+        setMessage({ type: "success", message: "Email send successfully!" });
+        form.current?.reset();
+        setSending(false);
+      })
+      .catch((error) =>
+        setMessage({
+          type: "error",
+          message: "Failed to sent email. Please try again later.",
+        })
+      );
+  };
+
   return (
     <div
       id="contact"
@@ -11,17 +43,46 @@ export default function Contact() {
         Get in Touch
       </div>
       <p className="mb-5 text-gray-500 dark:text-gray-400">
-        Have questions or suggestions? We&apos;d love to hear from you. Reach
-        out to us through any of these channels.
+        Have questions or suggestions? We&apos;d love to hear from you.
       </p>
-      <div className="flex items-center justify-center gap-7 dark:text-gray-300">
-        <a href="#" className="hover:text-red-600">
-          <LuMail size={30} />
-        </a>
-        <a href="#" className="hover:text-red-600">
-          <FaXTwitter size={30} />
-        </a>
-      </div>
+      {message && (
+        <div
+          className={`py-2 ${message.type === "success" ? "bg-green-100 text-green-600" : "bg-red-100 text-red-600"}`}
+        >
+          {message.message}
+        </div>
+      )}
+
+      <form onSubmit={handleEmailSend} ref={form}>
+        <input
+          type="text"
+          name="from_name"
+          className="my-2 w-full rounded-md border border-gray-200 p-3 dark:text-gray-300"
+          placeholder="Your name"
+          required
+        />
+
+        <input
+          type="email"
+          name="from_email"
+          className="my-2 w-full rounded-md border border-gray-200 p-3 dark:text-gray-300"
+          placeholder="Your email address"
+          required
+        />
+        <textarea
+          name="message"
+          placeholder="Your message"
+          className="my-2 w-full rounded-md border border-gray-200 p-3 dark:text-gray-300"
+          required
+        ></textarea>
+        <button
+          type="submit"
+          disabled={sending}
+          className={`w-full cursor-pointer rounded-md bg-red-600 px-6 py-3 text-xl font-medium text-white hover:bg-red-700 ${sending ? "opacity-40" : ""}`}
+        >
+          {sending ? "Sending" : "Send"}
+        </button>
+      </form>
     </div>
   );
 }
--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,9 @@
 const envVars = {
   WEATHER_API_KEY: import.meta.env.VITE_WEATHER_API_KEY,
+  EMAIL_SERVICE_ID: import.meta.env.VITE_EMAIL_SERVICE_ID,
+  EMAIL_TEMPLATE_ID: import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+  EMAIL_PUBLIC_KEY: import.meta.env.VITE_EMAIL_PUBLIC_KEY,
+  TO_EMAIL: import.meta.env.VITE_TO_EMAIL,
 };
 
 Object.freeze(envVars);
